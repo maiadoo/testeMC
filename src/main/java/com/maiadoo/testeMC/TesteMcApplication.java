745f65128bf15d2220a2de7e99dac779fccfe7d9
Maia do Ó <maianaradoo@gmail.com>
2019-05-27 16:52:49 -0300

    Criar primeiro projeto Spring Boot

---
+++ src/main/java/com/maiadoo/testeMC/TesteMcApplication.java
@@ -0,0 +1,13 @@
+package com.maiadoo.testeMC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteMcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteMcApplication.class, args);
+	}
+
+}
