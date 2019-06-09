3fcc7ea9de886a5861030c48ccb5ffd2427ac9c7
Maia do Ó <maianaradoo@gmail.com>
2019-06-09 18:37:18 -0300

    Operação de instanciação criada

--- src/main/java/com/maiadoo/testeMC/TesteMcApplication.java
+++ src/main/java/com/maiadoo/testeMC/TesteMcApplication.java
@@ -1,13 +1,33 @@
 package com.maiadoo.testeMC;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class TesteMcApplication {
+import com.maiadoo.testeMC.domain.Categoria;
+import com.maiadoo.testeMC.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class TesteMcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TesteMcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		//Instanciar os objetos de Categoria
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.save(Arrays.asList(cat1, cat2)); //Salvando em lista
+		
 	}
 
 }
